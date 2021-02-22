b221008590d07c134da427272d8acb6ca2d07b1c
Yuneider <yparada22mail.com>
2021-02-22 10:13:36 -0500

    HotelDAO, hotel completos

--- src/java/negocio/Hotel.java
+++ src/java/negocio/Hotel.java
@@ -9,5 +9,37 @@ public class Hotel {
 
     public Hotel() {
     }
+
+    public String getIdDescuento() {
+        return idDescuento;
+    }
+
+    public void setIdDescuento(String idDescuento) {
+        this.idDescuento = idDescuento;
+    }
+
+    public int getDias() {
+        return dias;
+    }
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+
+    public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    public String getCausante() {
+        return causante;
+    }
+
+    public void setCausante(String causante) {
+        this.causante = causante;
+    }
     
 }
