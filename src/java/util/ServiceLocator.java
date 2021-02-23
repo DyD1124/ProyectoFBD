739388cf2b07d974eb75a2392c2fc2ce79167dc9
Johan <johan.ebratt@gmail.com>
2021-02-22 21:16:14 -0500

    Recorderis de gastarle algo a milena

--- src/java/util/ServiceLocator.java
+++ src/java/util/ServiceLocator.java
@@ -23,7 +23,7 @@ public class ServiceLocator {
     
     private ServiceLocator() throws Exception{
         Class.forName("org.postgresql.Driver").newInstance();
-        conexion=DriverManager.getConnection("jdbc:postgresql://localhost:5432/ProyectoFinal","postgres","macanuda1205");
+        conexion=DriverManager.getConnection("jdbc:postgresql://localhost:5432/ProyectoFinal","postgres","Johan-10");
         conexion.setAutoCommit(false);        
     }
     
